db637c74bf19c04552ffdc0824dba84c4149e30b
Antraxmin <antraxmin@naver.com>
2022-10-31 23:34:03 +0900

    회원가입 UI - Joinform.js Joinform.css 임시 페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Loginform from './loginform';
+import Joinform from './Joinform';
 import React from 'react';
 
 function App() {
   return (
     <div>
       <Loginform />
+      {/* <Joinform /> */}
     </div>
   );
 }
